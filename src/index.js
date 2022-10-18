b3c0502f07e604e28ae3bc9f2c03ec52a81298f5
Tatyana <pereverzeva68@gmail.com>
2022-10-18 20:42:02 +0300

    кнопки удаления и +-

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"; 
+import App from "./app";
 
-import Counter from "./components/counter";
-
-const App = () => {
-    return <Counter />;
-}
 
 reactDom.render(<App />, document.getElementById("root"));
